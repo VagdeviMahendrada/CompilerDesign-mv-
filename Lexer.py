e9534bc66705a507e6fff1856a1df120687b7e02
vagdevi <vagde@vagdevi.(none)>
2023-10-03 22:15:15 +0530

    errors in parser

--- Lexer.py
+++ Lexer.py
@@ -3,9 +3,9 @@ class CLexer(Lexer):
     tokens={'INT','PRINT','ID','NUMBER'}
     literals={',',';','{','}','(',')','='}
     ignore=' \n\t'
+    ID=r'[a-zA-Z_][a-zA-Z0-9_]*'
     INT=r'int'
     PRINT=r'print'
-    ID=r'[a-zA-Z_][a-zA-Z0-9_]*'
     NUMBER=r'\d+'
     def __init__(self):
         self.lineno=1
